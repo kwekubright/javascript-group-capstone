b4d8b8355235272a4f63f0d1f2b9ce6b57e3a137
Kweku Bright <kweku01bright@gmail.com>
2022-06-20 13:21:38 +0100

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './docs',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new dotenv()
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'docs'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
