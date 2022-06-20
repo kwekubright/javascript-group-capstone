486db15f08f6c43be8bc0885ea5a2b600e92893b
Kweku Bright <kweku01bright@gmail.com>
2022-06-20 13:51:48 +0100

    Update HTML template

---
+++ src/modules/renderDocument.mod.js
@@ -0,0 +1,15 @@
+import { RenderElement } from "./RenderElement.mod";
+
+export const renderDocument = () => {
+  const documentBody = document.querySelector('body');
+  documentBody.innerHTML = '';
+
+  let sections = [];
+
+  sections.forEach(section => {
+    let renderElement = new RenderElement(section, documentBody);
+    renderElement.render();
+  });
+
+  loadScoreBoardFromService();
+}
