3653a991403081ed3ab95c4012925ce6f99c8362
Kweku Bright <kweku01bright@gmail.com>
2022-06-20 14:41:07 +0100

    Fix linters

--- src/modules/renderDocument.mod.js
+++ src/modules/renderDocument.mod.js
@@ -1,14 +1,1 @@
-import RenderElement from "./RenderElement.mod";
-
-export const renderDocument = () => {
-  const documentBody = document.querySelector('body');
-  documentBody.innerHTML = '';
-
-  let sections = [];
-
-  sections.forEach(section => {
-    let renderElement = new RenderElement(section, documentBody);
-    renderElement.render();
-  });
-
-}
+export const renderDocument = () => {}
