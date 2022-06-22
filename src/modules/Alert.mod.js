08380ae87db7969d3fa2cf6b99e3dec464111c73
Kweku Bright <kweku01bright@gmail.com>
2022-06-22 16:25:26 +0100

    Implement add comment form

---
+++ src/modules/Alert.mod.js
@@ -0,0 +1,35 @@
+export default class Alert { 
+  constructor(message, type, parent) {
+    this.message = message;
+    this.type = type;
+    this.parent = parent;
+  }
+
+  show() {
+    this.parent.innerHTML = "";
+    const alert = document.createElement('div');
+    alert.classList.add('alert');
+    alert.classList.add('flex');
+    alert.classList.add('flex-justify-center');
+    alert.classList.add(`alert-${this.type}`);
+    alert.innerHTML = `
+      <p>${this.message}</p>
+    `;
+    this.parent.appendChild(alert);
+
+    setTimeout(() => {
+      alert.remove();
+    }, 3000);
+    
+  }
+
+  static success(message, parent) {
+    const alert = new Alert(message, 'success', parent);
+    alert.show();
+  }
+
+  static error(message, parent) {
+    const alert = new Alert(message, 'error', parent);
+    alert.show();
+  }
+}
--- src/modules/ApiServices.mod.js
+++ src/modules/ApiServices.mod.js
@@ -1,1 +1,29 @@
-export default class ApiServices {}
+import { involvementApiBaseURL, involvementApiAppId } from "./variables";
+import Alert from "./Alert.mod";
+
+export default class ApiServices {
+  static pushComment = (id, comment, username) => {
+    const url = `${involvementApiBaseURL}apps/${involvementApiAppId}/comments`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: id,
+        username: username,
+        comment: comment
+      }),
+    };
+    fetch(url, options)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 201) {
+          Alert.success("Comment added successfully", document.querySelector('.alert-container'));
+        }
+        else {
+          Alert.error("Something went wrong", document.querySelector('.alert-container'));
+        }
+      });
+  }
+}
