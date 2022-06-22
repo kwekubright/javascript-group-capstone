0a7264a4e3052f94d8a5ac3acf580bf4f07fcbc4
Kweku Bright <kweku01bright@gmail.com>
2022-06-22 21:56:00 +0100

    Fix linter errors

--- __test__/commentCounter.test.js
+++ __test__/commentCounter.test.js
@@ -1,38 +1,35 @@
-import { commentCount } from "../src/modules/ApiServices.mod.js";
+/* eslint-disable no-unused-expressions */
+import { commentCount } from '../src/modules/ApiServices.mod.js';
 
-
-let mockAPIResponse = [
+const mockAPIResponse = [
   {
-    "comment": "This is nice!",
-    "creation_date": "2021-01-10",
-    "username": "John"
+    comment: 'This is nice!',
+    creation_date: '2021-01-10',
+    username: 'John',
   },
   {
-    "comment": "Great content!",
-    "creation_date": "2021-02-10",
-    "username": "Jane"
+    comment: 'Great content!',
+    creation_date: '2021-02-10',
+    username: 'Jane',
   },
   {
-    "comment": "This is great!",
-    "creation_date": "2021-03-10",
-    "username": "John"
+    comment: 'This is great!',
+    creation_date: '2021-03-10',
+    username: 'John',
   },
   {
-    "comment": "This is nice!",
-    "creation_date": "2021-04-10",
-    "username": "Jane"
-  }
+    comment: 'This is nice!',
+    creation_date: '2021-04-10',
+    username: 'Jane',
+  },
 ];
 
-
-describe("commentCount", () => {
-  test("should return the number of comments as 4", () => {
+describe('commentCount', () => {
+  test('should return the number of comments as 4', () => {
     expect(commentCount(mockAPIResponse)).toBe(4);
   });
 
-  test("should return the number of comments as 0", () => {
+  test('should return the number of comments as 0', () => {
     expect(commentCount([])).toBe(0);
   });
-
 });
-    
