12e329dd6e76b2cc13f90f77f18be93258a6d15f
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-22 12:39:48 +0100

    Add and export new variable

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -2,10 +2,12 @@ const baseUrl = 'https://api.tvmaze.com/search/shows?q=girls';
 const likesApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/30k7UkPeJYbggXrWr3FT/likes';
 const showcase = document.querySelector('#showcase');
 const allHearts = document.querySelector('#showcase');
+const movieNav = document.querySelector('.movie-nav')
 
 export {
   baseUrl,
   showcase,
   likesApi,
   allHearts,
+  movieNav,
 };
