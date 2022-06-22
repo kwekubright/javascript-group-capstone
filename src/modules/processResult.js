2c4aafa1bae2320b3badd78fd694bded4cecc88a
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-22 09:31:27 +0100

    Append item ID to all video likes

--- src/modules/processResult.js
+++ src/modules/processResult.js
@@ -2,6 +2,7 @@ import { showcase } from './variables.js';
 
 const processResult = (prop) => {
   for (let i = 0; i < prop.length; i += 1) {
+    let id = i;
     const { show } = prop[i];
     showcase.innerHTML += `
   <div class="movie">
@@ -9,7 +10,7 @@ const processResult = (prop) => {
     <div>
       <p class="title">${show.name}</p>
       <div class="likes">
-        <i class="bi bi-heart"></i>
+        <i class="bi bi-heart" id="item-${id}"></i>
         <small>0 likes</small>
       </div>
     </div>
