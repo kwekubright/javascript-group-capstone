d54da0033aa3410a1a612c790f11789fd3d3032c
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-22 12:42:34 +0100

    Import new functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+// import fetchLikes from './modules/fetchLikes';
 import './styles/index.css';
+import Like from './modules/postLikes';
 import display from './modules/display.js';
 import { showcase } from './modules/variables.js';
 
 showcase.innerHtml = display();
+
+let postLike = new Like;
+
+postLike.post();
