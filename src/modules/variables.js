a88aaffd5d264065b8fed83e92b9d784015cc45d
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-22 09:32:35 +0100

    Declear variable to grab all likes

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -1,7 +1,11 @@
 const baseUrl = 'https://api.tvmaze.com/search/shows?q=girls';
+const likesApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/30k7UkPeJYbggXrWr3FT/likes';
 const showcase = document.querySelector('#showcase');
+const allHearts = document.querySelector('#showcase');
 
 export {
   baseUrl,
   showcase,
+  likesApi,
+  allHearts,
 };
