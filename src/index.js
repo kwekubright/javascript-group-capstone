ed7fc3195fb19702a48ad3a12602f6d8aa4df99a
Kweku Bright <kweku01bright@gmail.com>
2022-06-22 12:02:28 +0100

    Update modal popup. Add Item details

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import display from './modules/display.js';
 import { showcase } from './modules/variables.js';
 
 showcase.innerHtml = display();
+
+const closeBtn = document.querySelector('.close-modal');
+closeBtn.addEventListener('click', () => {
+  document.getElementById('movie-modal').classList.add('hide');
+});
--- src/modules/processResult.js
+++ src/modules/processResult.js
@@ -1,5 +1,24 @@
 import { showcase } from './variables.js';
 
+const registerEvents = () => {
+  const commentBtn = document.querySelectorAll('.movie-comment');
+  commentBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      fetch(`https://api.tvmaze.com/shows/${e.currentTarget.dataset.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          document.getElementById('modal-movie-img').src = data.image.original;
+          document.getElementById('modal-movie-title').innerHTML = data.name;
+          document.getElementById('modal-movie-release-date').innerHTML = data.premiered;
+          document.getElementById('modal-movie-description').innerHTML = data.summary;
+          document.getElementById('modal-movie-rating').innerHTML = data.rating.average;
+          document.getElementById('modal-movie-language').innerHTML = data.language;
+          document.getElementById('movie-modal').classList.remove('hide');
+        });
+    });
+  });
+};
+
 const processResult = (prop) => {
   for (let i = 0; i < prop.length; i += 1) {
     const { show } = prop[i];
@@ -13,11 +32,13 @@ const processResult = (prop) => {
         <small>0 likes</small>
       </div>
     </div>
-    <button>Comments</button>
+    <button class="movie-comment" data-id="${show.id}" data-title="${show.name}" data-img="${show.image.original}">Comments</button>
     <button>Reservations</button>
   </div>
   `;
   }
+
+  registerEvents();
 };
 
 export default processResult;
